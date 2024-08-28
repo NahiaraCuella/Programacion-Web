57c813a7694202d9619a189792d92776aa23430d
NahiaraCuella <nahiaracuella16@gmail.com>
2024-08-28 17:08:18 -0300

    modifique los colores del portfolio y agregue una foto

--- portfolio/portfolio-nahi/src/components/Projects.jsx
+++ portfolio/portfolio-nahi/src/components/Projects.jsx
@@ -6,7 +6,9 @@ const Projects = () => {
       <h2 className="projects-title">Proyectos</h2>
       <div className="project">
         <h3>Proyecto 1</h3>
-        <p>nose.</p>
+        <p>Este proyecto es una cartelera de peliculas</p>
+        <img src="/cartelera.png" alt="foto" className="tamaño"/>
+        <a href ="http://127.0.0.1:5500/peliculas/index.html"> Ver proyecto</a>
       </div>
       <div className="project">
         <h3>Proyecto 2</h3>
