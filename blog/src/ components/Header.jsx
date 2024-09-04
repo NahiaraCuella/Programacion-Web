53602b0bc8c4c0b288a7da0e76cffb3c3b54cec6
NahiaraCuella <nahiaracuella16@gmail.com>
2024-09-04 17:00:11 -0300

    Empece hacer el blog

---
+++ blog/src/ components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Mi Blog de Viajes</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#home">Inicio</a>
+          </li>
+          <li>
+            <a href="#about">Sobre mí</a>
+          </li>
+          <li>
+            <a href="#contact">Contacto</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
